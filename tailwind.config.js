6d763f46c11435361b6920c038e624090fabb114
Sergej Moor <sergejmoor01@gmail.com>
2024-11-15 18:01:57 +0100

    feat: add tailwind and gsap

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  preprocess: vitePreprocess(),
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
